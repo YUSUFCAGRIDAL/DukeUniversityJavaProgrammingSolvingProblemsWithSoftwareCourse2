7c79cf52871f8d9d5b7aeb7fa34d60059fa21f8a
YUSUF CAGRI DAL <95309437+YUSUFCAGRIDAL@users.noreply.github.com>
2022-09-15 16:43:28 +0300

    Update FirstCSVExample.java

--- 06_Project_FirstCSV/FirstCSVExample.java
+++ 06_Project_FirstCSV/FirstCSVExample.java
@@ -1,18 +1,18 @@
 /**
  * Reads a chosen CSV file of our preferences and prints each field.
  * 
- * @author Duke Software Team
+ * @author Yusuf Çağrı DAL
  */
 import edu.duke.*;
 import org.apache.commons.csv.*;
 
 public class FirstCSVExample {
 	public void readFood() {
-		FileResource fr = new FileResource();
-		CSVParser parser = fr.getCSVParser();
+		FileResource fr = new FileResource(); //FilesResource is empty because it allows us to chose the file we want
+		CSVParser parser = fr.getCSVParser(); //it gives us CSV Parser
 		for (CSVRecord record : parser){
-			System.out.print(record.get("Name") + " ");
-			System.out.print(record.get("Favorite Color") + " ");
+			System.out.print(record.get("Name") + " "); // get us the field "Name"
+			System.out.print(record.get("Favorite Color") + " "); 
 			System.out.println(record.get("Favorite Food"));
 		}
 	}
