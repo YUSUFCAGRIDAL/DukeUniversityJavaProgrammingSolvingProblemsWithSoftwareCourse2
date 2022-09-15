d48de1c33a40dce726ce599a5c13d79fca2a94fd
YUSUF CAGRI DAL <95309437+YUSUFCAGRIDAL@users.noreply.github.com>
2022-09-15 13:21:27 +0300

    Update FindGeneWhile.java

--- 05_Project_FindGeneWhile/FindGeneWhile.java
+++ 05_Project_FindGeneWhile/FindGeneWhile.java
@@ -8,21 +8,22 @@
 public class FindGeneWhile {
     
     public String findGene(String dna) {
-        int startIndex =dna.indexOf("ATG");
-        int currIndex =dna.indexOf("TAA", startIndex +3);
-        while (currIndex != -1) {
-            if((currIndex - startIndex) % 3 == 0){
+        int startIndex =dna.indexOf("ATG"); //find the first occurance of ATG and call it startIndex
+        int currIndex =dna.indexOf("TAA", startIndex +3); //look for TAA after ATG call it currIndex
+        
+        while (currIndex != -1) { //if ATG doeasn't appear in our DNA, doesn't start the program
+            if((currIndex - startIndex) % 3 == 0){ //check if the distance between ATG and TAA is multiple of 3
                 
-                return dna.substring (startIndex, currIndex + 3);
+                return dna.substring (startIndex, currIndex + 3); // finishes the method and gives the DNA
             
             }
             else {
-                currIndex = dna.indexOf("TAA", currIndex + 3);
+                currIndex = dna.indexOf("TAA", currIndex + 3); //continue to look for right TAA and update the currIndex
                 
             }
         }
         
-        return "";
+        return ""; //no gene found. because no TAA found
     }
     
     public void testFindGeneSimple(){
